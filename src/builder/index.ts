3cadffc9756bb3b320c867e404095b46c5a2a052
Pablo Bacchetta <polbac@gmail.com>
2019-06-13 20:12:09 -0300

    builder

--- src/builder/index.ts
+++ src/builder/index.ts
@@ -7,12 +7,52 @@ class BannerHappybirthdate {
         this.firstName = firstName
         this.lastName = lastName
         this.years = years
-        
-        return `
+    }
+
+    showBanner() {
+        console.log(`
             🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂
             🎂🎂 ${this.firstName} ${this.lastName} happy birthdate (${this.years}) 🎂🎂🎂
             🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂
-        `
+        `)
     }
 
+
 }
+
+
+class BannerHappybirthdateBuilder {
+    firstName: string
+    lastName: string
+    years: number
+
+    withFirtName(name: string) {
+        this.firstName = name
+        return this
+    }
+
+    withLastName(lastName: string) {
+        this.lastName = lastName
+        return this
+    }
+
+    withYears(years: number) {
+        this.years = years
+        return this
+    }
+
+    build() {
+        const birthdate = new BannerHappybirthdate(this.firstName, this.lastName, this.years)
+        return birthdate.showBanner()
+    }
+}
+
+/* const banner = new BannerHappybirthdate('John', 'Adams', 70)
+banner.showBanner() */
+
+const bannerWithBuilder = new BannerHappybirthdateBuilder()
+bannerWithBuilder
+    .withFirtName('John')
+    .withLastName('Adams')
+    .withYears(70)
+    .build()
